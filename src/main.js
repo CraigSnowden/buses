4de3710e9e8994f8e89c43520500a00f1d53e6a4
Craig Snowden <craig@craigsnowden.com>
2019-03-28 18:47:20 +0000

    Add fontawesome for icons and use official Lothian Buses colours

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBus, faTrain } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faBus, faTrain);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
